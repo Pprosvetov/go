b676136313b47689e3c959e715d488a5925bbb68
pavelprosvetov <pavelprosvetov@gmail.com>
2021-05-09 13:56:56 +0300

    Lec3 commit

---
+++ Lec2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	var (
+		word1 = "имя"
+		word2 = "твое"
+		word3 = "мне"
+		word4 = "знакомо"
+	)
+
+	fmt.Printf("%s %s %s %s", word1, word2, word3, word4)
+}
